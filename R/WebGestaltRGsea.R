7684b209888cc4b635d317d4d147af7bbf97df37
Yuxing Liao <yuxingliao@gmail.com>
2018-10-18 16:23:05 -0500

    Add setNum parameter of target number for set cover to return.

--- R/WebGestaltRGsea.R
+++ R/WebGestaltRGsea.R
@@ -1,4 +1,4 @@
-WebGestaltRGsea <- function(organism="hsapiens", enrichDatabase="geneontology_Biological_Process", enrichDatabaseFile=NULL, enrichDatabaseType=NULL, enrichDatabaseDescriptionFile=NULL,  interestGeneFile=NULL, interestGene=NULL, interestGeneType=NULL, collapseMethod="mean", minNum=10, maxNum=500, fdrMethod="BH", sigMethod="fdr", fdrThr=0.05, topThr=10, dNum=20, perNum=1000, isOutput=TRUE, outputDirectory=getwd(), projectName=NULL, dagColor="binary", nThreads=1, hostName="http://www.webgestalt.org/") {
+WebGestaltRGsea <- function(organism="hsapiens", enrichDatabase="geneontology_Biological_Process", enrichDatabaseFile=NULL, enrichDatabaseType=NULL, enrichDatabaseDescriptionFile=NULL,  interestGeneFile=NULL, interestGene=NULL, interestGeneType=NULL, collapseMethod="mean", minNum=10, maxNum=500, fdrMethod="BH", sigMethod="fdr", fdrThr=0.05, topThr=10, dNum=20, setNum=10, perNum=1000, isOutput=TRUE, outputDirectory=getwd(), projectName=NULL, dagColor="binary", nThreads=1, hostName="http://www.webgestalt.org/") {
 	enrichMethod <- "GSEA"
 
 	if(is.null(projectName)){
@@ -119,7 +119,7 @@ WebGestaltRGsea <- function(organism="hsapiens", enrichDatabase="geneontology_Bi
 			fdr[fdr == 0] <- .Machine$double.eps
 			signedLogFdr <- -log(fdr) * sign(enrichedSig$ES)
 			apRes <- affinityPropagation(idsInSet, signedLogFdr)
-			wscRes <- weightedSetCover(idsInSet, 1 / signedLogFdr, 10, nThreads)
+			wscRes <- weightedSetCover(idsInSet, 1 / signedLogFdr, setNum, nThreads)
 			writeLines(sapply(apRes$clusters, paste, collapse="\t"), file.path(projectDir, paste0("enriched_geneset_ap_clusters_", projectName, ".txt")))
 			writeLines(c(paste0("# Coverage: ", wscRes$coverage), wscRes$topSets), file.path(projectDir, paste0("enriched_geneset_wsc_topsets_", projectName, ".txt")))
 			clusters$ap <- apRes
