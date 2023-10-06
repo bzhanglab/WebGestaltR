9105be24f08825277a8a8f5236266c1447526e8a
John Elizarraras <elizarrj27@gmail.com>
2023-10-06 16:38:22 -0500

    add rewrite of fill_input_matrix
    fix typo

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# WebGestalt
+
 WebGestalt R package is the R version of our well-known web application tool WebGestalt (www.webgestalt.org) that has on average 27,000 users from 140 countries and territories per year and has also been cited 371 in 2016. The advantage of this R package is that it can be easily integrated to other pipelines or simultaneously analyze multiple gene lists.
 
 WebGestaltR function can perform popular enrichment analyses: ORA (Over-Representation Analysis), GSEA (Gene Set Enrichment Analysis) and NTA (Network Topology Analysis). Based on the user-uploaded gene list or gene list with scores (for GSEA method), WebGestaltR function will first map the gene list to entrez gene IDs and then summarize the gene list based on the GO (Gene Ontology) Slim data. After performing the enrichment analysis, WebGestaltR function also returns an user-friendly HTML report containing GO Slim summary and enrichment analysis result. If the functional categories have the DAG (directed acyclic graph) structure, the structure of the enriched categories can also be visualized in the report.
+
+## Installation
+
+Since WebGestaltR v2.0.0, Rust is used for core computations in the R package. Therefore, to install WebGestaltR, please download and install Rust from [https://www.rust-lang.org/learn/get-started](https://www.rust-lang.org/learn/get-started). For Mac, Linux, or Unix users, Rust can be installed from the command line, and Windows users can download a GUI installer.
--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,12 @@
 use extendr_api::prelude::*;
+use extendr_api::wrapper::dataframe::Dataframe;
+use ndarray::Array2;
+use rustc_hash::FxHashMap;
 use webgestalt_lib::methods::*;
-
 /// Return string `"Hello world!"` to R.
 /// @export
 #[extendr]
-fn hello_world() -> &'static str {
+fn rust_hello_world() -> &'static str {
     "Hello world!"
 }
 
@@ -15,10 +17,52 @@ fn gsea_rust() -> () {
     // webgestalt_lib::methods::gsea::
 }
 
+/// Fill relation data frame for GSEA input
+///
+/// Fill 1 for gene in gene set
+/// ## Diagram
+/// ```shell
+///         Gene Sets
+///       ┌───────────┐  First column named 'gene' containing gene name
+///       │A0100110100│  1 = in set
+/// Genes │B0100101000│  0 = not in set
+///       │C1011101001│
+///       └───────────┘
+/// ```
+/// @param gmt A Data Frame with geneSet and gene columns from the GMT file
+/// @param genes A vector of genes
+/// @param geneSets A vector of gene sets
+///
+/// @return A Data Frame with the first column of gene and 1 or 0 for other columns of gene sets.
+/// @author John Elizarraras
+/// @keywords internal
+/// @export
+#[extendr]
+pub fn fill_input_data_frame(gmt: Robj, genes: Robj, gene_sets: Robj) -> Robj {
+    let genes_vec = genes.as_string_vector().unwrap();
+    let mut gene_set_vec = gene_sets.as_string_vector().unwrap();
+    let mut value_array = Array2::zeros((genes.len(), gmt.len()));
+    let mut geneIndex: FxHashMap<&String, usize> = FxHashMap::default();
+    let mut setIndex: FxHashMap<&String, usize> = FxHashMap::default();
+    let gmtSet: Vec<String> = gmt.index("geneSet").unwrap().as_string_vector().unwrap();
+    let gmtGene: Vec<String> = gmt.index("gene").unwrap().as_string_vector().unwrap();
+    for (i, val) in genes_vec.iter().enumerate() {
+        geneIndex.insert(val, i);
+    }
+    for (i, val) in gene_set_vec.iter().enumerate() {
+        setIndex.insert(val, i);
+    }
+    for i in 0..gmtSet.len() {
+        value_array[[geneIndex[&gmtGene[i]], setIndex[&gmtSet[i]]]] = 1;
+    }
+    gene_set_vec.insert(0, String::from("gene"));
+    data_frame!(x = 1)
+}
+
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod WebGestaltR;
-    fn hello_world;
+    fn rust_hello_world;
 }
