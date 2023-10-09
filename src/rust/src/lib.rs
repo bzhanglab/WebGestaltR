f8f17cc2cdef673bc775bdbecbe917a4eaadb693
John Elizarraras <elizarrj27@gmail.com>
2023-10-09 11:56:11 -0500

    adds fill_input_data_frame, but returns list

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -17,9 +17,17 @@ fn gsea_rust() -> () {
     // webgestalt_lib::methods::gsea::
 }
 
+
+enum SafeTypes {
+    String(String),
+    Int(i32)
+}
 /// Fill relation data frame for GSEA input
 ///
 /// Fill 1 for gene in gene set
+///
+/// See https://github.com/extendr/extendr/issues/612 for how to export DataFrame
+///
 /// ## Diagram
 /// ```shell
 ///         Gene Sets
@@ -38,25 +46,32 @@ fn gsea_rust() -> () {
 /// @keywords internal
 /// @export
 #[extendr]
-pub fn fill_input_data_frame(gmt: Robj, genes: Robj, gene_sets: Robj) -> Robj {
+pub fn fill_input_data_frame(gmt: Robj, genes: Robj, gene_sets: Robj) -> List {
     let genes_vec = genes.as_string_vector().unwrap();
-    let mut gene_set_vec = gene_sets.as_string_vector().unwrap();
+    let gene_set_vec = gene_sets.as_string_vector().unwrap();
     let mut value_array = Array2::zeros((genes.len(), gmt.len()));
-    let mut geneIndex: FxHashMap<&String, usize> = FxHashMap::default();
-    let mut setIndex: FxHashMap<&String, usize> = FxHashMap::default();
-    let gmtSet: Vec<String> = gmt.index("geneSet").unwrap().as_string_vector().unwrap();
-    let gmtGene: Vec<String> = gmt.index("gene").unwrap().as_string_vector().unwrap();
+    let mut gene_index: FxHashMap<&String, usize> = FxHashMap::default();
+    let mut set_index: FxHashMap<&String, usize> = FxHashMap::default();
+    let gmt_set: Vec<String> = gmt.index("geneSet").unwrap().as_string_vector().unwrap();
+    let gmt_gene: Vec<String> = gmt.index("gene").unwrap().as_string_vector().unwrap();
     for (i, val) in genes_vec.iter().enumerate() {
-        geneIndex.insert(val, i);
+        gene_index.insert(val, i);
     }
     for (i, val) in gene_set_vec.iter().enumerate() {
-        setIndex.insert(val, i);
+        set_index.insert(val, i);
     }
-    for i in 0..gmtSet.len() {
-        value_array[[geneIndex[&gmtGene[i]], setIndex[&gmtSet[i]]]] = 1;
+    for i in 0..gmt_set.len() {
+        value_array[[gene_index[&gmt_gene[i]], set_index[&gmt_set[i]]]] = 1;
     }
-    gene_set_vec.insert(0, String::from("gene"));
-    data_frame!(x = 1)
+    let mut gene_set_val: Vec<Vec<i32>> = Vec::new();
+    // gene_set_val.push(genes_vec.into_iter().map(|x| SafeTypes::String(x)).collect());
+    for i in 0..gmt_set.len() {
+        gene_set_val.push(value_array.column(i).to_vec())
+    }
+    // gene_set_vec.insert(0, String::from("gene"));
+    // Construct DataFrame in R. Create list for now.
+    List::from_names_and_values(gene_set_vec, gene_set_val).unwrap()
+    // data_frame!(x = 1)
 }
 
 // Macro to generate exports.
