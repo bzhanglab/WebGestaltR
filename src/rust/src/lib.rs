644b3a93d90910fbd194c464646a1260efc17fbf
John Elizarraras <elizarrj27@gmail.com>
2023-10-09 12:08:19 -0500

    add basic output for GSEA

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,5 +1,6 @@
+use std::vec;
+
 use extendr_api::prelude::*;
-use extendr_api::wrapper::dataframe::Dataframe;
 use ndarray::Array2;
 use rustc_hash::FxHashMap;
 use webgestalt_lib::methods::*;
@@ -11,17 +12,18 @@ fn rust_hello_world() -> &'static str {
 }
 
 /// Run GSEA using rust library
+/// @return List of the results of GSEA
 /// @export
 #[extendr]
-fn gsea_rust() -> () {
+fn gsea_rust() -> List {
     // webgestalt_lib::methods::gsea::
+    list!(
+        fdr = vec![0.01, 0.05, 0.1],
+        leading_edge = vec![4, 6, 4],
+        gene_sets = vec!["GO1", "GO2", "GO3"]
+    )
 }
 
-
-enum SafeTypes {
-    String(String),
-    Int(i32)
-}
 /// Fill relation data frame for GSEA input
 ///
 /// Fill 1 for gene in gene set
@@ -34,12 +36,13 @@ enum SafeTypes {
 ///       ┌───────────┐  First column named 'gene' containing gene name
 ///       │A0100110100│  1 = in set
 /// Genes │B0100101000│  0 = not in set
-///       │C1011101001│
-///       └───────────┘
+///       │C1011101001│  Due to limitiations with extendr-api v 0.6.0,
+///       └───────────┘  function returns a list, and the R package will
+///                      add the first 'gene' column
 /// ```
 /// @param gmt A Data Frame with geneSet and gene columns from the GMT file
 /// @param genes A vector of genes
-/// @param geneSets A vector of gene sets
+/// @param gene_sets A vector of gene sets
 ///
 /// @return A Data Frame with the first column of gene and 1 or 0 for other columns of gene sets.
 /// @author John Elizarraras
