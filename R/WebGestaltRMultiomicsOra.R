b419281d62cab902d0184a045d6b69d805bdee2b
John Elizarraras <elizarrj27@gmail.com>
2024-03-06 11:35:40 -0600

    more fixes

--- R/WebGestaltRMultiomicsOra.R
+++ R/WebGestaltRMultiomicsOra.R
@@ -12,6 +12,7 @@ WebGestaltRMultiOmicsOra <- function(analyteLists = NULL, analyteListFiles = NUL
   projectDir <- file.path(outputDirectory, paste0("Project_", projectName))
   cat("Performing multi-omics ORA\nLoading the functional categories...\n")
   all_sets <- .load_meta_gmt(enrichDatabase, enrichDatabaseFile, enrichDatabaseDescriptionFile, enrichDatabaseType, analyteLists, analyteListFiles, analyteTypes, organism, cache, hostName)
+  print(all_sets)
   cat("Loading the ID lists...\n")
   interest_lists <- list()
   interestGeneMaps <- list()
